0b1cfbe8d4a1de4ffd258939b0c9cc384a9b8c6f
Dimitar Nonov <d.nonov@gmail.com>
2019-09-15 23:18:05 +0100

    Experiment with django url system

--- django_react/urls.py
+++ django_react/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     # have it as the last urlpattern for BrowserHistory urls to work
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
